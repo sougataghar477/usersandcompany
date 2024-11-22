597f36ad8899cda3b5c7433b0f975dc3e10d5f76
sougata <sougataghar47@gmail.com>
2024-11-22 14:54:12 +0530

    added users route

--- app/page.js
+++ app/page.js
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid place-items-center h-screen">
-      <div><h1 className="text-4xl mb-4">Users and Company Management</h1>
+    <div className="grid place-items-center h-screen px-2 py-4 md:py-8">
+      <div className="boxShadow p-4 lg:p-16 rounded-lg">
+        <h1 className="text-4xl mb-4  ">Users and Company Management</h1>
         <Link
           className="text-blue-500 mr-4 hover:text-blue-700 hover:underline transition duration-300"
           href={'/company'}>
